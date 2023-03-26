389fbfc2df3d00e219ad6fe799dd69035158e9ac
Lancelot de Briey <Business@lancedb.dev>
2023-03-27 00:34:03 +0200

    added basic files; and working on the styles

---
+++ apps/web/src/components/layout/header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return <div>Header</div>;
+};
+
+const HeaderManifesto = (props: Props) => {
+  return <div className="h- bg-vdao-dark">Header Manifest</div>;
+};
+
+export { HeaderManifesto, Header };
+
+export default Header;
