7105bfced51556d94089b705b7f1dec0203bf3f6
Lancelot de Briey <Business@lancedb.dev>
2023-04-13 08:21:14 +0200

    refactoring

--- apps/web/src/pages/about.tsx
+++ apps/web/src/pages/about.tsx
@@ -1,13 +1,16 @@
 import { type NextPage } from "next";
+
 import Footer from "~/components/layout/footer";
 import Header from "~/components/layout/header";
-import { MailingListComponent } from "~/components/misc/mailinglist";
+
 import {
   AboutUsComponent,
   AboutUsCoreTeamComponent,
   AboutUsCoreValueComponent,
   AboutUsVisionMissionComponent,
 } from "~/components/pages/about";
+
+import { MailingListComponent } from "~/components/misc/mailinglist";
 
 const AboutUs: NextPage = () => {
   return (
