dcc3a9dff1d8938f3506cb9bcaf7e3cf47c317a9
prasadbhanu703 <prasadbhanu703@gmail.com>
2023-04-29 22:17:22 -0400

    Memeber Support page is done

--- apps/web/src/pages/app/support.tsx
+++ apps/web/src/pages/app/support.tsx
@@ -1,5 +1,6 @@
 import Footer from "~/components/layout/footer";
 import Header from "~/components/layout/header";
+import ContentsTable from "~/components/pages/app/support/contentsTable";
 import MemberSuport from "~/components/pages/app/support/memberSupport";
 
 const Support = () => {
@@ -9,6 +10,7 @@ const Support = () => {
 
       <div className="w-full text-vdao-dark">
         <MemberSuport />
+        <ContentsTable />
       </div>
 
       <Footer />
