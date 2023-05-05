de7d6254bbca9179fa5fb68835a2bc563740cf17
obirijah <tobirijah@gmail.com>
2023-05-05 00:55:44 -0700

    ui fix

--- apps/web/src/components/misc/mailinglist.tsx
+++ apps/web/src/components/misc/mailinglist.tsx
@@ -5,31 +5,34 @@ const MailingListComponent = () => {
   return (
     <div className={`bg-vdao-lightpurple`}>
       <div
-        className={`${containerClass} mx-auto max-w-[1280px] bg-vdao-lightpurple md:flex-row md:py-24`}
+        className={`mx-auto max-w-[1280px] bg-vdao-lightpurple px-6 py-14 md:flex-row md:py-24`}
       >
         <div className="flex-1">
           <div className="font-heading text-5xl font-medium">
             Join Our
             <br /> Mailing List
           </div>
-          <div className="pt-8 pr-32 text-2xl font-normal md:pt-9">
+        </div>
+
+        <div className="justify-between md:flex md:pt-8">
+          <div className="pt-8 pr-32 text-2xl font-normal md:w-6/12 md:pt-0">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit morbi turpis
             mi.
           </div>
-        </div>
 
-        <div className="flex flex-1 flex-col justify-end pt-8 md:flex-row md:pt-24">
-          <input
-            placeholder="Enter your email"
-            className="h-10 w-full rounded-md pl-5 text-vdao-dark outline-none outline-vdao-dark placeholder:text-vdao-dark placeholder:opacity-50"
-          />
-          <DarkButton
-            text="Subscribe"
-            className="mt-6 h-10 py-2 font-heading md:mt-0 md:ml-5"
-          />
+          <div className="flex flex-1 flex-col justify-end pt-8 md:flex-row md:pt-0">
+            <input
+              placeholder="Enter your email"
+              className="h-10 w-full rounded-md pl-5 text-vdao-dark outline-none outline-vdao-dark placeholder:text-vdao-dark placeholder:opacity-50"
+            />
+            <DarkButton
+              text="Subscribe"
+              className="mt-6 h-10 py-2 font-heading md:mt-0 md:ml-5"
+            />
+          </div>
         </div>
       </div>
-      <div className="hidden">
+      {/* <div className="hidden">
         <div className="font-heading text-5xl font-medium">
           Join Our Mailing List
         </div>
@@ -51,7 +54,7 @@ const MailingListComponent = () => {
             />
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
