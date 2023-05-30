31878c660f8a0b449031c1707245cacaf9194011
Brieyla <business@lancedb.dev>
2023-05-30 11:25:53 +0200

    parsed prettier to every ts file that wasn't up to standards

--- apps/web/src/components/pages/app/grants/popups/team.ts
+++ apps/web/src/components/pages/app/grants/popups/team.ts
@@ -1,33 +1,33 @@
-import Icon4 from "public/icons/pods/icon4.svg";
-import Icon5 from "public/icons/pods/icon5.svg";
-import Icon6 from "public/icons/pods/icon6.svg";
-import Icon7 from "public/icons/pods/icon7.svg";
-import Icon8 from "public/icons/pods/icon8.svg";
+import Icon4 from 'public/icons/pods/icon4.svg'
+import Icon5 from 'public/icons/pods/icon5.svg'
+import Icon6 from 'public/icons/pods/icon6.svg'
+import Icon7 from 'public/icons/pods/icon7.svg'
+import Icon8 from 'public/icons/pods/icon8.svg'
 
 export const TeamDetails = [
   {
     icon: Icon4,
-    name: "JustinCase",
-    contributed: "0.5 ETH",
+    name: 'JustinCase',
+    contributed: '0.5 ETH',
   },
   {
     icon: Icon5,
-    name: "StableLAb",
-    contributed: "0.1 ETH",
+    name: 'StableLAb',
+    contributed: '0.1 ETH',
   },
   {
     icon: Icon6,
-    name: "coldiron.eth",
-    contributed: "0.1 ETH",
+    name: 'coldiron.eth',
+    contributed: '0.1 ETH',
   },
   {
     icon: Icon7,
-    name: "ChicagoDAO",
-    contributed: "0.5 ETH",
+    name: 'ChicagoDAO',
+    contributed: '0.5 ETH',
   },
   {
     icon: Icon8,
-    name: "Onestone",
-    contributed: "0.5 ETH",
+    name: 'Onestone',
+    contributed: '0.5 ETH',
   },
-];
+]
--- apps/web/src/components/pages/nftCollections/faqs.ts
+++ apps/web/src/components/pages/nftCollections/faqs.ts
@@ -1,27 +1,27 @@
 export const FAQS = [
   {
-    title: "Morbi turpis mi, faucibus vitae.",
+    title: 'Morbi turpis mi, faucibus vitae.',
     description:
-      "Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.",
+      'Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.',
   },
   {
-    title: "Aenean venenatis erat eget placerat.",
+    title: 'Aenean venenatis erat eget placerat.',
     description:
-      "Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.",
+      'Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.',
   },
   {
-    title: "Vivamus facilisis odio non orci malesuada.",
+    title: 'Vivamus facilisis odio non orci malesuada.',
     description:
-      "Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.",
+      'Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.',
   },
   {
-    title: "Morbi turpis mi, faucibus vitae.",
+    title: 'Morbi turpis mi, faucibus vitae.',
     description:
-      "Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.",
+      'Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.',
   },
   {
-    title: "Aenean venenatis erat eget placerat.",
+    title: 'Aenean venenatis erat eget placerat.',
     description:
-      "Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.",
+      'Sed quis sapien nec erat blandit cursus eget at risus. Cras maximus orci ultricies mi tempor pellentesque.',
   },
-];
+]
