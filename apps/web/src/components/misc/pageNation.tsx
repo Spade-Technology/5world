a9cb7c7b7da8d41be7288c5495c03eaac8af78c0
obirijah <tobirijah@gmail.com>
2023-06-05 00:32:17 -0700

    blog page modification

--- apps/web/src/components/misc/pageNation.tsx
+++ apps/web/src/components/misc/pageNation.tsx
@@ -1,63 +1,54 @@
-import Image from "next/image";
-import LeftArrow from "public/icons/pageNation/leftArrow.svg";
-import RightArrow from "public/icons/pageNation/rightArrow.svg";
-import DarkLeftAroow from "public/icons/pageNation/darkLeftArrow.svg";
-import DarkRightArrow from "public/icons/pageNation/darkRight.svg";
+import Image from 'next/image'
+import LeftArrow from 'public/icons/pageNation/leftArrow.svg'
+import RightArrow from 'public/icons/pageNation/rightArrow.svg'
+import DarkLeftAroow from 'public/icons/pageNation/darkLeftArrow.svg'
+import DarkRightArrow from 'public/icons/pageNation/darkRight.svg'
 
 type PageNationProps = {
-  pageNumbers: any;
-  pageCount: number;
-  setPageCount: any;
-  web3?: boolean;
-};
+  pageNumbers: any
+  pageCount: number
+  setPageCount: any
+  web3?: boolean
+}
 
-const PageNation = ({
-  pageNumbers,
-  pageCount,
-  setPageCount,
-  web3,
-}: PageNationProps) => {
+const PageNation = ({ pageNumbers, pageCount, setPageCount, web3 }: PageNationProps) => {
   return (
-    <div className="flex w-full justify-end px-6">
-      <div className="flex justify-center py-11 md:justify-end md:py-14 ">
+    <div className='flex w-full justify-end px-6'>
+      <div className='flex justify-center py-11 md:justify-end md:py-14 '>
         <Image
           src={web3 ? DarkLeftAroow : LeftArrow}
-          alt="Left Arrow"
+          alt='Left Arrow'
           height={10.61}
           width={10.61}
-          className="mr-5 cursor-pointer"
+          className='mr-5 cursor-pointer'
           onClick={() => pageCount > 1 && setPageCount(pageCount - 1)}
         />
         {pageNumbers.length &&
           pageNumbers.map((number: number, idx: number) => {
             return (
               <div
-                className={`cursor-pointer pr-4 text-lg ${
-                  pageCount === number
-                    ? " text-vdao-light"
-                    : `${web3 ? "text-white" : ""}`
+                className={`cursor-pointer pr-4 font-body text-lg font-normal ${
+                  pageCount === number ? ' text-vdao-light' : `${web3 ? 'text-white' : ''}`
                 }`}
                 onClick={() => setPageCount(number)}
                 key={idx}
               >
                 {number}
               </div>
-            );
+            )
           })}
 
         <Image
           src={web3 ? DarkRightArrow : RightArrow}
-          alt="Right Arrow"
+          alt='Right Arrow'
           height={10.61}
           width={10.61}
-          className="cursor-pointer"
-          onClick={() =>
-            pageCount < pageNumbers.length && setPageCount(pageCount + 1)
-          }
+          className='cursor-pointer'
+          onClick={() => pageCount < pageNumbers.length && setPageCount(pageCount + 1)}
         />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PageNation;
+export default PageNation
--- apps/web/src/pages/blog/index.tsx
+++ apps/web/src/pages/blog/index.tsx
@@ -1,34 +1,28 @@
-import Header from "~/components/layout/header";
-import DownwardsArrow from "public/icons/blog/polygonDownwards.svg";
-import Image from "next/image";
+import Header from '~/components/layout/header'
+import DownwardsArrow from 'public/icons/blog/polygonDownwards.svg'
+import Image from 'next/image'
 
-import MailingListComponent from "~/components/misc/mailinglist";
-import Footer from "~/components/layout/footer";
-import BlogPagination from "~/components/pages/blog/blogPagination";
+import MailingListComponent from '~/components/misc/mailinglist'
+import Footer from '~/components/layout/footer'
+import BlogPagination from '~/components/pages/blog/blogPagination'
 
 const Blog = () => {
   return (
     <>
       <Header web2 />
-      <div className="mx-auto max-w-[1280px] bg-white  text-vdao-dark">
-        <h1 className="px-6 pt-16 pb-10 text-8xl font-medium md:pb-0">Blog</h1>
+      <div className='mx-auto max-w-[1280px] bg-white  text-vdao-dark'>
+        <h1 className='px-6 pt-16 pb-10 text-[44px] font-medium md:pb-0 md:text-8xl'>Blog</h1>
 
-        <div className="flex w-full flex-col-reverse justify-end gap-3 py-10 px-6 md:mr-20 md:flex-row md:px-0 md:py-16">
-          <div className="flex h-10 w-full cursor-pointer justify-between rounded-md bg-vdao-dark px-5 py-1  text-white md:mx-0 md:w-48 md:py-2">
-            <span className="my-auto text-lg">Catagory</span>
-            <Image
-              src={DownwardsArrow}
-              alt=""
-              height={8}
-              width={10}
-              className="my-auto"
-            />
+        <div className='flex w-full flex-col-reverse justify-end gap-3 py-10 px-6 md:mr-20 md:flex-row md:px-5 md:py-16 xl:px-0'>
+          <div className='mx-0 flex h-10 w-full cursor-pointer items-center justify-between rounded-md bg-vdao-dark  px-5 text-white md:w-48'>
+            <div className='my-auto font-body text-lg font-normal'>Catagory</div>
+            <Image src={DownwardsArrow} alt='' height={8} width={10} className='my-auto' />
           </div>
 
-          <div>
+          <div className='w-full'>
             <input
-              placeholder="Search"
-              className="h-10 w-full rounded-md pl-5 text-vdao-dark outline-none outline-1 outline-vdao-dark placeholder:text-vdao-dark placeholder:opacity-30 md:mx-0 md:w-56"
+              placeholder='Search'
+              className='h-9 w-full rounded-[5px] px-5 text-vdao-dark outline-none outline-1 outline-vdao-dark placeholder:text-vdao-dark placeholder:opacity-30 md:mx-0 md:w-56'
             />
           </div>
         </div>
@@ -39,7 +33,7 @@ const Blog = () => {
 
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default Blog;
+export default Blog
