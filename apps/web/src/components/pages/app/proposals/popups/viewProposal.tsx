dd23704f7d43098f8da6558a3a93e3ef1e42bc97
prasadbhanu703 <prasadbhanu703@gmail.com>
2023-06-09 19:06:05 -0400

    Proposals hooks integrated except create proposal

--- apps/web/src/components/pages/app/proposals/popups/viewProposal.tsx
+++ apps/web/src/components/pages/app/proposals/popups/viewProposal.tsx
@@ -3,26 +3,32 @@ import CustomModal from '~/components/misc/customModal'
 import ProfileCard from '~/components/misc/profileCard'
 import Image from 'next/image'
 import { SupporterDetails } from './details'
+import { useProposalRead } from '~/hooks/web3/useProposal'
+import { monthNames } from '~/utils/date'
+import DummyIcon from 'public/icons/pods/icon1.svg'
 
 type ViewProposalProps = {
   show: boolean
   close: any
+  proposalID: number
 }
 
-const ViewProposal = ({ show, close }: ViewProposalProps) => {
+const ViewProposal = ({ show, close, proposalID }: ViewProposalProps) => {
   const [showComments, setShowComments] = useState(false)
+  const { data: proposal } = useProposalRead(proposalID)
+
   return (
     <CustomModal show={show} close={close}>
       <div className='pb-[30px] font-body text-lg font-normal text-vdao-dark'>
-        <div className='font-bold'>Posted FEB 02 2023</div>
+        <div className='font-bold'>
+          {proposal?.createdAt ? 'Posted ' + monthNames[proposal.createdAt.getUTCMonth()] + ' ' + proposal.createdAt.getDate() + ', ' + proposal.createdAt.getFullYear() : 'at Unavailable'}
+        </div>
 
         <div className='grid grid-cols-1 gap-[73px] py-[10px] md:grid-cols-3 md:py-5'>
           <div className='col-span-2'>
-            <div className='font-heading text-[26px] font-medium md:text-[30px]'>
-              Proposal name goes here. One or two sentence will be ideal for this section
-            </div>
+            <div className='font-heading text-[26px] font-medium md:text-[30px]'>{proposal ? proposal.title : 'No title'}</div>
             <div className='pt-[10px] md:pt-5'>
-              <ProfileCard />
+              <ProfileCard icon={proposal ? proposal.picture : DummyIcon} name={proposal ? proposal.name : 'Unnamed'} address={proposal?.authorId} />
             </div>
 
             <div className={` mt-[25px] flex flex-col gap-[22px] md:mt-11 md:flex-row md:gap-20`}>
@@ -43,16 +49,10 @@ const ViewProposal = ({ show, close }: ViewProposalProps) => {
             </div>
 
             <div className='flex gap-[10px] border-b-[1px] border-b-vdao-dark pb-2 pt-10 font-body font-bold'>
-              <div
-                className={` ${!showComments && 'text-vdao-light'} cursor-pointer justify-start`}
-                onClick={() => setShowComments(false)}
-              >
+              <div className={` ${!showComments && 'text-vdao-light'} cursor-pointer justify-start`} onClick={() => setShowComments(false)}>
                 Proposal Detail
               </div>
-              <div
-                className={` ${showComments && 'text-vdao-light'} cursor-pointer justify-start`}
-                onClick={() => setShowComments(true)}
-              >
+              <div className={` ${showComments && 'text-vdao-light'} cursor-pointer justify-start`} onClick={() => setShowComments(true)}>
                 Comments (5)
               </div>
             </div>
@@ -85,13 +85,11 @@ const ViewProposal = ({ show, close }: ViewProposalProps) => {
 const Detailed = () => {
   return (
     <div className='pt-11 text-black'>
-      The Governance Facilitator(s) and the Protocol Engineering Core Unit have placed an urgent out-of-schedule
-      executive proposal into the voting system. MKR Holders should vote for this proposal if they support the following
-      alterations to the Maker Protocol.
+      The Governance Facilitator(s) and the Protocol Engineering Core Unit have placed an urgent out-of-schedule executive proposal into the voting system. MKR Holders should vote for this proposal if
+      they support the following alterations to the Maker Protocol.
       <br />
       <br />
-      If you are new to voting in the Maker Protocol, please see the voting guide to learn how voting works, and this
-      wallet setup guide to set up your wallet to vote.
+      If you are new to voting in the Maker Protocol, please see the voting guide to learn how voting works, and this wallet setup guide to set up your wallet to vote.
       <br />
       <br />
       Executive Summary
@@ -104,20 +102,17 @@ const Detailed = () => {
       Voting for this executive proposal will place your MKR in support of the changes and additions outlined above.
       <br />
       <br />
-      Unless otherwise noted, the changes and additions listed above are subject to the GSM Pause Delay. This means that
-      if this executive proposal passes, the changes and additions listed above will only become active in the Maker
-      Protocol after the GSM Pause Delay has expired. The GSM Pause Delay is currently set to 48 hours.
+      Unless otherwise noted, the changes and additions listed above are subject to the GSM Pause Delay. This means that if this executive proposal passes, the changes and additions listed above will
+      only become active in the Maker Protocol after the GSM Pause Delay has expired. The GSM Pause Delay is currently set to 48 hours.
       <br />
       <br />
-      If this executive proposal does not pass within 30 days, then it will expire and can no longer have any effect on
-      the Maker Protocol.
+      If this executive proposal does not pass within 30 days, then it will expire and can no longer have any effect on the Maker Protocol.
       <br />
       <br />
       Urgent Collateral Parameter Proposal Details
       <br />
-      As per this successful urgent signal request, the following parameter changes will take place if this
-      out-of-schedule executive proposal passes. To read more on the process for urgent responses, please see MIP24:
-      Emergency Response.
+      As per this successful urgent signal request, the following parameter changes will take place if this out-of-schedule executive proposal passes. To read more on the process for urgent responses,
+      please see MIP24: Emergency Response.
       <br />
       <br />
       MATIC-A Changes
@@ -151,18 +146,15 @@ const Detailed = () => {
       <br />
       Review
       <br />
-      Community debate on these topics can be found on the MakerDAO Governance forum. Please review any linked threads
-      to inform your position before voting.
+      Community debate on these topics can be found on the MakerDAO Governance forum. Please review any linked threads to inform your position before voting.
       <br />
       <br />
-      Additionally, these changes may have been discussed further in recent Governance calls. Video for these calls is
-      available to review.
+      Additionally, these changes may have been discussed further in recent Governance calls. Video for these calls is available to review.
       <br />
       <br />
       Resources
       <br />
-      Additional information about the Governance process can be found in the Governance section of the MakerDAO
-      community portal.
+      Additional information about the Governance process can be found in the Governance section of the MakerDAO community portal.
       <br />
       <br />
       To participate in future Governance calls, please join us every Thursday at 17:00 UTC.
--- apps/web/src/components/pages/app/proposals/proposalCards.tsx
+++ apps/web/src/components/pages/app/proposals/proposalCards.tsx
@@ -1,79 +1,74 @@
-import Image from 'next/image'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import PageNation from '~/components/misc/pageNation'
 import PrimaryButton from '~/styles/shared/buttons/primaryButton'
-import { proposalDetails } from './proposalDetails'
+// import { proposalDetails } from './proposalDetails'
 import ProfileCard from '~/components/misc/profileCard'
-import { useProposalRead, useProposalReads } from '~/hooks/web3/useProposal'
+import { useProposalReads } from '~/hooks/web3/useProposal'
+import { monthNames } from '~/utils/date'
+import DummyIcon from 'public/icons/pods/icon1.svg'
 
 type ProposalProps = {
   setViewProposal: Dispatch<SetStateAction<boolean>>
+  setProposalID: Dispatch<SetStateAction<number>>
 }
 
 type CardProps = {
   proposal: any
   setViewProposal: Dispatch<SetStateAction<boolean>>
+  setProposalID: Dispatch<SetStateAction<number>>
 }
 
-const ProposalCards = ({ setViewProposal }: ProposalProps) => {
+const ProposalCards = ({ setProposalID, setViewProposal }: ProposalProps) => {
   const [pageCount, setPageCount] = useState(1)
   const [pageNumbers, setPageNumbers] = useState<any>([])
-  const [proposals, setUpdatedProposals] = useState<any>([])
-  const { data } = useProposalReads([0, 1, 2, 3, 4], { pod: true, author: true })
-
-  console.log('proposal data', data)
+  const [updatedproposals, setUpdatedProposals] = useState<any>([])
+  const { data } = useProposalReads({ include: { author: true, pod: true } })
   const itemsPerPage = 3
 
   /** The following two useEffects are for Pagenation functionality. */
   useEffect(() => {
-    if (proposalDetails.length) {
+    if (data) {
       let pageCountArr = []
       let count = 0
-      for (let i = 0; i < proposalDetails.length; i++) {
+      for (let i = 0; i < data?.length; i++) {
         if ((i + 1) % itemsPerPage === 0) {
           count = count + 1
           pageCountArr.push(count)
         }
       }
 
-      if (proposalDetails.length % itemsPerPage !== 0) {
+      if (data.length % itemsPerPage !== 0) {
         pageCountArr.push(count + 1)
       }
 
       setPageNumbers(pageCountArr)
     }
-  }, [proposalDetails.length])
+  }, [data])
 
   useEffect(() => {
-    if (pageCount) {
+    if (pageCount && data) {
       let updatedBlogsArr = []
       const startBlog = itemsPerPage * (pageCount - 1)
-      const endBlog =
-        (pageCount - 1) * itemsPerPage + 3 <= proposalDetails.length
-          ? (pageCount - 1) * itemsPerPage + 3
-          : proposalDetails.length
+      const endBlog = (pageCount - 1) * itemsPerPage + 3 <= data.length ? (pageCount - 1) * itemsPerPage + 3 : data.length
 
       for (let i = startBlog; i < endBlog; i++) {
-        updatedBlogsArr.push(proposalDetails[i])
+        updatedBlogsArr.push(data[i])
       }
 
       setUpdatedProposals(updatedBlogsArr)
     }
-  }, [pageCount])
+  }, [pageCount, data])
 
   return (
     <div className='mx-auto w-screen bg-vdao-deep'>
       <div className='mx-auto max-w-[1280px] pb-[120px]'>
-        <div
-          id='allProposals'
-          className='mx-6 max-w-[1280px] font-heading text-[32px] font-medium text-vdao-light md:mx-auto md:text-[46px]'
-        >
+        <div id='allProposals' className='mx-6 max-w-[1280px] font-heading text-[32px] font-medium text-vdao-light md:mx-auto md:text-[46px]'>
           Proposals
         </div>
 
         <div className='mx-6 mt-[15px]  md:mx-0 '>
-          {proposals?.map((proposal: any, idx: number) => {
-            return <Card proposal={proposal} key={idx} setViewProposal={setViewProposal} />
+          {updatedproposals?.map((proposal: any, idx: number) => {
+            return <Card proposal={proposal} key={idx} setViewProposal={setViewProposal} setProposalID={setProposalID} />
           })}
         </div>
 
@@ -83,28 +78,29 @@ const ProposalCards = ({ setViewProposal }: ProposalProps) => {
   )
 }
 
-export const Card = ({ proposal, setViewProposal }: CardProps) => {
+export const Card = ({ proposal, setViewProposal, setProposalID }: CardProps) => {
   return (
     <div className='mt-[20px] rounded-[20px] bg-white px-5 py-10 font-body text-vdao-dark md:mt-[30px] md:p-[50px]'>
       <div className='flex flex-col md:flex-row'>
         <div className='flex-1'>
-          <div className='text-lg font-semibold'>{proposal.postedAt}</div>
-          <div className='pt-[10px] text-3xl font-medium'>{proposal.content}</div>
-
-          <ProfileCard icon={proposal.creatorIcon} name={proposal.createdName} Address={proposal.creatorAddress} />
-
-          <div
-            className={`mt-[30px] w-fit cursor-pointer rounded-[20px] border-[1px] border-vdao-dark px-9 py-[5px] text-lg font-medium text-vdao-light`}
-          >
-            Active
+          <div className='text-lg font-semibold'>
+            {proposal?.createdAt ? 'Posted ' + monthNames[proposal.createdAt.getUTCMonth()] + ' ' + proposal.createdAt.getDate() + ', ' + proposal.createdAt.getFullYear() : 'at Unavailable'}
           </div>
+          <div className='pt-[10px] text-3xl font-medium'>{proposal?.title}</div>
+
+          <ProfileCard icon={proposal?.author?.picture ? proposal?.author?.picture : DummyIcon} name={proposal?.author?.name} address={proposal?.author?.address} />
+
+          <div className={`mt-[30px] w-fit cursor-pointer rounded-[20px] border-[1px] border-vdao-dark px-9 py-[5px] text-lg font-medium text-vdao-light`}>Active</div>
         </div>
         <div className='flex-1'>
           <div className='pt-7 text-lg font-normal md:pt-9'>{proposal.description}</div>
           <PrimaryButton
             text='View Detail'
             className='mt-[30px] py-[5px] px-9 text-xl font-medium'
-            onClick={() => setViewProposal(true)}
+            onClick={() => {
+              setProposalID(proposal?.id)
+              setViewProposal(proposal)
+            }}
           />
         </div>
       </div>
