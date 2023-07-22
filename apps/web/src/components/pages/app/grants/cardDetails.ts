5d9c0a8b5de32048279f4c8a00f33fbffbabeb56
Brieyla <business@lancedb.dev>
2023-07-22 14:56:42 +0200

    published an explainative version of the grant details

--- apps/web/src/components/pages/app/grants/cardDetails.ts
+++ apps/web/src/components/pages/app/grants/cardDetails.ts
@@ -5,42 +5,69 @@ import Image4 from 'public/illustrations/grants/image4.svg'
 
 export const GrantDetails = [
   {
-    id: 1,
-    title: 'Current Round: 100K for Soil Research',
-    proposedBy: 'Brieyla',
-    rules: [
+    id: 3,
+    title: 'Current Round: 300K for Soil Research',
+    proposedBy: 'Bhanu',
+    rules:
       'This page contains details of the projects which have been selected to receive grants from the DAO following the application phase. DAO funding received by each proposal is proportionate to the number of votes received from within the community.',
 
-      'DAO following the application phase. DAO funding received by each ',
-    ],
-    status: 'Active',
-    votingPower: '123M',
-    currentGrants : [
+    //   enum RoundState {
+    //     Pending,
+    //     ApplicationActive,
+    //     ApplicationReview,
+    //     Active,
+    //     Completed,
+    //     FundsAllocated
+    //     Canceled,
+    //     Defeated,
+    //     Succeeded,
+    //     Queued,
+    //     Expired,
+    //     Executed
+    // } (id: 0 is Pending, 1 is ApplicationActive, 2 is ApplicationReview, 3 is Active, 4 is Completed, 5 is FundsAllocated, etc...)
+    status: 1,
+
+    // when people can create their project. if it's not within that time, the people shouldn't be able to create a project
+    applicationStartBlock: 1234567890n,
+    applicationEndBlock: 1234567890n,
+
+    // This is the voting time. If it's not within that time, the people shouldn't be able to vote
+    roundStartBlock: 1234567890n,
+    roundEndBlock: 1234567890n,
+
+    totalVotes: 1234567890n,
+    quadraticVotes: 12356789n,
+
+    // The matching amount is in wei (10^18 or anything else depending on the token decimals)
+    matchingAmount: 1234567890n,
+    tokenAddress: '0x1234567890',
+
+    requests: [
       {
-        image: Image1,
+        image: Image3,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '2 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image2,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
-      },
-      {
-        image: Image3,
-        title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '10 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image4,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
-    ]
+      {
+        image: Image1,
+        title: 'Grant Title Goes Here',
+        lastUpdated: '100 day ago', // convert to block
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
+      },
+    ],
   },
   {
     id: 2,
@@ -51,72 +78,99 @@ export const GrantDetails = [
 
       'DAO following the application phase. DAO funding received by each ',
     ],
-    status: 'Active',
-    votingPower: '234M',
-    currentGrants : [
+
+    votingPower: 1000000n,
+    currentGrants: [
       {
         image: Image2,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image3,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image4,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image1,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
-    ]
+    ],
   },
   {
     id: 3,
     title: 'Current Round: 300K for Soil Research',
     proposedBy: 'Bhanu',
-    rules: [
+    rules:
       'This page contains details of the projects which have been selected to receive grants from the DAO following the application phase. DAO funding received by each proposal is proportionate to the number of votes received from within the community.',
 
-      'DAO following the application phase. DAO funding received by each ',
-    ],
-    status: 'Closed',
-    votingPower: '1000M',
-    currentGrants : [
+    //   enum RoundState {
+    //     Pending,
+    //     ApplicationActive,
+    //     ApplicationReview,
+    //     Active,
+    //     Completed,
+    //     FundsAllocated
+    //     Canceled,
+    //     Defeated,
+    //     Succeeded,
+    //     Queued,
+    //     Expired,
+    //     Executed
+    // } (id: 0 is Pending, 1 is ApplicationActive, 2 is ApplicationReview, 3 is Active, 4 is Completed, 5 is FundsAllocated, etc...)
+    status: 1,
+
+    // when people can create their project. if it's not within that time, the people shouldn't be able to create a project
+    applicationStartBlock: 1234567890n,
+    applicationEndBlock: 1234567890n,
+
+    // This is the voting time. If it's not within that time, the people shouldn't be able to vote
+    roundStartBlock: 1234567890n,
+    roundEndBlock: 1234567890n,
+
+    totalVotes: 1234567890n,
+    quadraticVotes: 12356789n,
+
+    // The matching amount is in wei (10^18 or anything else depending on the token decimals)
+    matchingAmount: 1234567890n,
+    tokenAddress: '0x1234567890',
+
+    requests: [
       {
         image: Image3,
         title: 'Grant Title Goes Here',
-        lastUpdated: '2 day ago',
+        lastUpdated: '2 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image2,
         title: 'Grant Title Goes Here',
-        lastUpdated: '10 day ago',
+        lastUpdated: '10 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image4,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image1,
         title: 'Grant Title Goes Here',
-        lastUpdated: '100 day ago',
+        lastUpdated: '100 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
-    ]
+    ],
   },
   {
     id: 4,
@@ -127,110 +181,103 @@ export const GrantDetails = [
 
       'DAO following the application phase. DAO funding received by each ',
     ],
-    status: 'Closed',
-    votingPower: '408M',
-    currentGrants : [
+    status: 'closed',
+    votingPower: 1234567890n,
+    currentGrants: [
       {
         image: Image1,
         title: 'Grant Title Goes Here',
-        lastUpdated: '21 day ago',
+        lastUpdated: '21 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image3,
         title: 'Grant Title Goes Here',
-        lastUpdated: '221 day ago',
+        lastUpdated: '221 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image2,
         title: 'Grant Title Goes Here',
-        lastUpdated: '221 day ago',
+        lastUpdated: '221 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image4,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
-    ]
+    ],
   },
   {
     id: 5,
     title: 'Current Round: 500K for Soil Research',
     proposedBy: 'PRabhu',
-    rules: [
-      'This page contains details of the projects which have been selected to receive grants from the DAO following the application phase. DAO funding received by each proposal is proportionate to the number of votes received from within the community.',
-
-      'DAO following the application phase. DAO funding received by each ',
-    ],
-    status: 'Active',
-    votingPower: '543M',
-    currentGrants : [
+    rules:
+      'This page contains details of the projects which have been selected to receive grants from the DAO following the application phase. DAO funding received by each proposal is proportionate to the number of votes received from within the community. DAO following the application phase. DAO funding received by each ',
+    status: 'active',
+    votingPower: 555555555n,
+    currentGrants: [
       {
         image: Image4,
         title: 'Grant Title Goes Here',
-        lastUpdated: '134 day ago',
+        lastUpdated: '134 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image2,
         title: 'Grant Title Goes Here',
-        lastUpdated: '154 day ago',
+        lastUpdated: '154 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image3,
         title: 'Grant Title Goes Here',
-        lastUpdated: '14 day ago',
+        lastUpdated: '14 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image1,
         title: 'Grant Title Goes Here',
-        lastUpdated: '1 day ago',
+        lastUpdated: '1 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
-    ]
+    ],
   },
   {
     id: 6,
     title: 'Current Round: 600K for Soil Research',
     proposedBy: 'Yash',
-    rules: [
-      'This page contains details of the projects which have been selected to receive grants from the DAO following the application phase. DAO funding received by each proposal is proportionate to the number of votes received from within the community.',
-
-      'DAO following the application phase. DAO funding received by each ',
-    ],
+    rules:
+      'This page contains details of the projects which have been selected to receive grants from the DAO following the application phase. DAO funding received by each proposal is proportionate to the number of votes received from within the community.  following the application phase. DAO funding received by each ',
     status: 'Active',
-    votingPower: '1M',
-    currentGrants : [
+    votingPower: 100000n,
+    currentGrants: [
       {
         image: Image1,
         title: 'Grant Title Goes Here',
-        lastUpdated: '12 day ago',
+        lastUpdated: '12 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image2,
         title: 'Grant Title Goes Here',
-        lastUpdated: '31 day ago',
+        lastUpdated: '31 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image3,
         title: 'Grant Title Goes Here',
-        lastUpdated: '31 day ago',
+        lastUpdated: '31 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
       {
         image: Image4,
         title: 'Grant Title Goes Here',
-        lastUpdated: '14 day ago',
+        lastUpdated: '14 day ago', // convert to block
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet elementum urna, in volutpat risus. Quisque nec tempus diam, sit amet luctus mi. ',
       },
-    ]
+    ],
   },
 ]
-
