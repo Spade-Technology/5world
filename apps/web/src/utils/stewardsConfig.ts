f0243f0f58bb967922443caa9ff81123a02f5659
Brieyla <business@lancedb.dev>
2023-07-26 04:36:50 +0200

    updated steward times

--- apps/web/src/utils/stewardsConfig.ts
+++ apps/web/src/utils/stewardsConfig.ts
@@ -5,5 +5,5 @@ dayjs.extend(duration)
 
 export const StewardUnit = 'second'
 export const ApplicationTime = dayjs.duration(5, 'minute')
-export const VotingTime = dayjs.duration(50000, 'minute')
+export const VotingTime = dayjs.duration(5, 'minute')
 export const TotalCycleTime = ApplicationTime.add(VotingTime)
